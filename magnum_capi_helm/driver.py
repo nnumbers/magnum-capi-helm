54aa00d69029fd19ee54aba4d0c1fcc8a772ffb3
Morgany Vieira Mendes <morgany@mangustotech.com>
2025-03-24 10:51:39 -0300

    replace call from _get_list_from_str to strutils.split_by_commas

--- magnum_capi_helm/driver.py
+++ magnum_capi_helm/driver.py
@@ -750,9 +750,9 @@ class Driver(driver.Driver):
         )
 
     def _get_allowed_cidrs(self, context, cluster):
-        allowed_cidr_list = self._get_list_from_str(
+        allowed_cidr_list = strutils.split_by_commas(
             CONF.capi_helm.api_master_lb_cloud_allowed_cidrs
-            ) + self._get_list_from_str(
+            ) + strutils.split_by_commas(
                 cluster.labels.get("api_master_lb_allowed_cidrs", ""))
         if len(allowed_cidr_list) > 0:
             subnet_cidr = self._label(cluster, "fixed_subnet_cidr", "10.0.0.0/24")
@@ -764,9 +764,6 @@ class Driver(driver.Driver):
         
         LOG.debug(f"CIDR list {allowed_cidr_list}")
         return allowed_cidr_list
-    
-    def _get_list_from_str(self,value):
-        return value.split(",") if isinstance(value, str) and value != "" else []
 
     def _storageclass_definitions(self, context, cluster):
         """Query cinder API to retrieve list of available volume types.
