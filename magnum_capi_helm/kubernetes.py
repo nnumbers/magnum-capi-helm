0308c0c12fc367dcfb43d3b3f1d0e4caf8af830d
Morgany Vieira Mendes <morgany@mangustotech.com>
2026-05-30 00:49:19 -0300

    update cluster api from v1beta2  to v1beta2
    see: https://cluster-api.sigs.k8s.io/reference/api/crd-api-reference

--- magnum_capi_helm/kubernetes.py
+++ magnum_capi_helm/kubernetes.py
@@ -286,23 +286,23 @@ class Secret(Resource):
 
 
 class Cluster(Resource):
-    api_version = "cluster.x-k8s.io/v1beta1"
+    api_version = "cluster.x-k8s.io/v1beta2"
 
 
 class OpenstackCluster(Resource):
-    api_version = "infrastructure.cluster.x-k8s.io/v1beta1"
+    api_version = "infrastructure.cluster.x-k8s.io/v1beta2"
 
 
 class MachineDeployment(Resource):
-    api_version = "cluster.x-k8s.io/v1beta1"
+    api_version = "cluster.x-k8s.io/v1beta2"
 
 
 class KubeadmControlPlane(Resource):
-    api_version = "controlplane.cluster.x-k8s.io/v1beta1"
+    api_version = "controlplane.cluster.x-k8s.io/v1beta2"
 
 
 class Machine(Resource):
-    api_version = "cluster.x-k8s.io/v1beta1"
+    api_version = "cluster.x-k8s.io/v1beta2"
 
 
 class Manifests(Resource):
